d62341a9e4f881a27060d2cd89687029a9cfc930
big_Xplosion <bigXplosion44@gmail.com>
2015-05-25 23:12:39 +0200

    main mod class

---
+++ src/main/scala/big_xplosion/retroactivegeneration/RetroactiveGeneration.scala
@@ -0,0 +1,24 @@
+package big_xplosion.retroactivegeneration
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent}
+
+@Mod(modid = RetroactiveGeneration.MOD_ID, name = RetroactiveGeneration.MOD_NAME, version = RetroactiveGeneration.MOD_VERSION)
+object RetroactiveGeneration {
+
+  final val MOD_ID = "RetroactiveGeneration"
+  final val MOD_NAME = "Retroactive Generation"
+  final val MOD_VERSION = "@VERSION@"
+
+  def preInit(event: FMLPreInitializationEvent) {
+
+  }
+
+  def init(event: FMLInitializationEvent) {
+
+  }
+
+  def postInit(event: FMLPostInitializationEvent) {
+
+  }
+}
